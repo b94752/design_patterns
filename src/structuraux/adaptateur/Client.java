938c0a9c19cd7a6f7e6ece417f6310a36881b13e
benoit.charroux <benoit.charroux@EFREI89124M2-P.campus.villejuif>
2018-05-28 12:01:59 +0200

    adapter

--- src/structuraux/adaptateur/Client.java
+++ src/structuraux/adaptateur/Client.java
@@ -3,8 +3,8 @@ package structuraux.adaptateur;
 public class Client {
 
 	public static void main(String[] args) {
-		Adapter adapter = new Adapter();
-		adapter.request();
+		Target target = new Adapter();
+		target.request();
 	}
 
 }
